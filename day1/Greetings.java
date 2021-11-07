4a2b4cbc302ae8b5cda5f9bee242e6e6b2399ea0
dilaram pokhrel <dila414@gmail.com>
2021-11-07 18:39:45 -0500

    First commit

---
+++ day1/Greetings.java
@@ -0,0 +1,23 @@
+package day1;
+
+public class Greetings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * this is my comment which is ignore by java compile
+		 */
+		System.out.println("Hello world java\nGreeting from my learning");
+		System.out.println("My Name is Rachana");
+		System.out.println("I live  in south ridding.");
+		System.out.println("I have two kids");
+
+		
+  
+
+
+	 
+	}
+
+}
